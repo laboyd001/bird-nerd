70a5790229650e3369ed48094cce1a15a2b4eaf8
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-12-11 14:36:01 -0600

    added some reactstrap to login and reg

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,43 +1,77 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input,
+  Container,
+  Button,
+  ButtonGroup
+} from "reactstrap";
 
 export default class Login extends Component {
-
   render() {
     return (
       <React.Fragment>
-        <div className={this.props.hideLoginForm ? "hide" : "bryans__class"}>
-          <div className="login__form">
-            <h2>Please sign in</h2>
-
-            <label className="formLabel" htmlFor="inputEmail">
-              Email address
-            </label>
-            <input className="formInput" onChange={this.props.handleFieldChange} type="email"
-              id="loginEmail"
-              placeholder="Email address"
-              required="" autoFocus="" />
-
-            <label className="formLabel" htmlFor="inputPassword">
-              Password
-            </label>
-            <input className="formInput" onChange={this.props.handleFieldChange} type="password"
-              id="loginPassword"
-              placeholder="Password"
-              required="" />
-              <br/>
-
-            <p className="formLabel">Remember me <input className="formInput" onChange={this.props.handleFieldChange} type="checkbox"
-              id="remember" /></p>
-
-            <button className="btn btn_mod" type="submit" onClick={() => {this.props.handleLogin()}}>
-              Sign in
-                </button>
+        <Container>
+          <div className={this.props.hideLoginForm ? "hide" : "login-form"}>
+            <div className="login-form">
+              <h2>Please sign in</h2>
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>Email Address</InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  onChange={this.props.handleFieldChange}
+                  type="email"
+                  id="loginEmail"
+                  placeholder="Email address"
+                  required=""
+                  autoFocus=""
+                />
+              </InputGroup>
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>Password</InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  onChange={this.props.handleFieldChange}
+                  type="password"
+                  id="loginPassword"
+                  placeholder="Password"
+                  required=""
+                />
+              </InputGroup>
+              <p className="formLabel">
+                Remember me
+                <input
+                  className="formInput"
+                  onChange={this.props.handleFieldChange}
+                  type="checkbox"
+                  id="remember"
+                />
+              </p>
+              <ButtonGroup>
+                <Button
+                  className="btn"
+                  type="submit"
+                  onClick={() => {
+                    this.props.handleLogin();
+                  }}
+                >
+                  Sign in
+                </Button>
+                <Button
+                  className="register__link btn"
+                  onClick={() => this.props.handleChangeForm()}
+                >
+                  Register an account
+                </Button>
+              </ButtonGroup>
+            </div>
           </div>
-          <div className="text__center">
-          <button  className="register__link" onClick={() => this.props.handleChangeForm()}>Register an account</button>
-          </div>
-        </div>
+        </Container>
       </React.Fragment>
-    )
+    );
   }
 }
--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -1,47 +1,96 @@
 import React, { Component } from "react"
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+  Input,
+  Container,
+  Button,
+  ButtonGroup
+} from "reactstrap";
 
 export default class Register extends Component {
 
   render() {
     return (
+      <Container>
       <div className={this.props.hideLoginForm ? "bryans__class" : "hide"}>
         <div className="login__form">
           <h2>Register Here</h2>
-          <label className="formLabel" htmlFor="inputName">
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>Username</InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  onChange={this.props.handleFieldChange}
+                  id="registerName"
+                  placeholder="Display Name"
+                  required=""
+                  autoFocus=""
+                />
+              </InputGroup>
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>Email Address</InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  onChange={this.props.handleFieldChange}
+                  type="email"
+                  id="registerEmail"
+                  placeholder="Email Address"
+                  required=""
+                  autoFocus=""
+                />
+              </InputGroup>
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText>Password</InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  onChange={this.props.handleFieldChange}
+                  type="password"
+                  id="registerPassword"
+                  placeholder="Password"
+                  required=""
+                />
+              </InputGroup>
+          {/* <label className="formLabel" htmlFor="inputName">
             Name
           </label>
           <input className="formInput" onChange={this.props.handleFieldChange} type="text"
             id="registerName"
             placeholder="Display Name"
-            required="" autoFocus="" />
+            required="" autoFocus="" /> */}
 
-          <label className="formLabel" htmlFor="inputEmail">
+          {/* <label className="formLabel" htmlFor="inputEmail">
             Email address
           </label>
           <input className="formInput" onChange={this.props.handleFieldChange} type="email"
             id="registerEmail"
             placeholder="Email address"
-            required="" autoFocus="" />
+            required="" autoFocus="" /> */}
 
-          <label className="formLabel" htmlFor="inputPassword">
+          {/* <label className="formLabel" htmlFor="inputPassword">
             Password
           </label>
           <input className="formInput" onChange={this.props.handleFieldChange} type="password"
             id="registerPassword"
             placeholder="Password"
-            required="" />
-
-          <button type="button" className="btn btn_mod" onClick={() => {
-            this.props.handleRegister()
-          }}>
-            Register
-          </button>
-        </div>
-
-        <div className="text__center">
-          <button className="register__link" onClick={() => this.props.handleChangeForm()}>Already have an account? Sign In</button>
-        </div>
+            required="" /> */}
+          <ButtonGroup>
+            <Button type="button" className="btn btn_mod" 
+              onClick={() => {
+              this.props.handleRegister()
+              }}>
+              Register
+            </Button>
+            <Button className="register__link" 
+              onClick={() => this.props.handleChangeForm()}>Already have an account? Sign In
+            </Button>
+          </ButtonGroup>
+          </div>
       </div>
+      </Container>
     )
   }
 }
