fb5f554fb63c6ae438a786c71107c428aa9aa9c7
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-12-11 09:37:24 -0600

    removed api key test

--- src/components/BirdNerd.js
+++ src/components/BirdNerd.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ApplicationViews from "./ApplicationViews"
 import './BirdNerd.css';
 
-console.log("test",process.env.REACT_APP_API_KEY_Google)
 export default class BirdNerd extends Component {
   isAuthenticated = () => (sessionStorage.getItem("credentials") !== null || localStorage.getItem("credentials") !== null)
 
