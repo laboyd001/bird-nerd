cc7410d7d4edb0e0167264e99945f06829d2873b
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-12-12 14:41:08 -0600

    card styling to the sighting list

--- src/components/sighting/SightingCard.js
+++ src/components/sighting/SightingCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Button, ButtonGroup } from 'reactstrap';
+import { Button, ButtonGroup, Card, CardText, CardBody,
+  CardTitle, CardSubtitle, Container } from 'reactstrap';
 import "./Sighting.css";
 import SightingEdit from "./SightingEdit";
 import Moment from 'react-moment';
@@ -8,25 +9,37 @@ import BirdCard from '../bird/BirdCard'
 export default class SightingCard extends Component {
   render() {
     return (
-      <React.Fragment>
-        <div className="sighting-card" key={this.props.sighting.id}>
-          <div className="sighting-nobird">
-          <h5>Date:<br></br></h5>
-          <p>
-          <Moment format="MMMM Do YYYY">
-          {this.props.sighting.date}
-          </Moment>
-          </p>
-          <h5>Location:<br></br></h5>
-          <p>{this.props.sighting.location}</p>
-          <h5>Bird:<br></br></h5>
-          <p>{this.props.sighting.bird.name}</p>
-          <h5>Summary:<br></br></h5>
-          <p>{this.props.sighting.summary}</p>
-        
-        
-        
-        <ButtonGroup className="card-button">
+      <Container>
+      <Card>
+        <CardBody>
+          {/* <CardTitle>
+          {this.props.sighting.bird.name}
+          </CardTitle> */}
+          <CardTitle>
+            {this.props.sighting.location}
+          </CardTitle>
+          <CardSubtitle>
+            <Moment format="MMMM Do YYYY">
+              {this.props.sighting.date}
+            </Moment>
+          </CardSubtitle>
+        </CardBody>
+        <CardBody>
+            {this.props.birds
+            .filter(bird=>bird.id=== this.props.sighting.birdId)
+            
+            .map(bird => (
+              <BirdCard key={bird.id} bird={bird} birds={this.props.birds} />
+            ))}
+        </CardBody>
+        <CardBody>
+          <CardSubtitle>
+            Sighting Summary:
+          </CardSubtitle>
+          <CardText>
+            {this.props.sighting.summary}
+          </CardText>
+          <ButtonGroup className="card-button">
             <SightingEdit
               birds={this.props.birds}
               sighting={this.props.sighting}
@@ -41,21 +54,14 @@ export default class SightingCard extends Component {
             >
               Delete
             </Button>
-        </ButtonGroup>
-        </div>
-        <section className="birds">
-          <div className="card__holder">
-            {this.props.birds
-            .filter(bird=>bird.id=== this.props.sighting.birdId)
-            
-            .map(bird => (
-              <BirdCard key={bird.id} bird={bird} birds={this.props.birds} />
-            ))}
-          </div>
-        </section>
+         </ButtonGroup>
+        </CardBody>
+      </Card>
+    </Container>
 
-        </div>
-      </React.Fragment>
+
+
+
     );
   }
 }
